cbd52be5c21f41d3cdb18158e1bafd943b3adb70
mazdato <toshihide.matsuda@gmail.com>
2022-05-09 22:57:41 +0900

    hamanu解法

--- 90mon/ABC/ABC200-299/ABC250/E.cpp
+++ 90mon/ABC/ABC200-299/ABC250/E.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h> 
+#include <unordered_map>
 using namespace std;
 
 typedef long long ll;
@@ -35,6 +36,42 @@ void solve() {
 
     cin >> N;
 
+    vector<ll> a(N), b(N);
+    vector<pair<ll,ll> > rA(N), rB(N);
+    
+    unordered_map<ll, ll> numMp;
+    rep(i, N) cin >> a[i];
+    rep(i, N) cin >> b[i];
+
+    ll val = 0;
+    
+    rep(i, N) if( !numMp.count(a[i]) ) numMp[a[i]] = val++;
+    rep(i, N) if( !numMp.count(b[i]) ) numMp[b[i]] = val++;
+
+    set<ll> sA,sB;
+    ll cntA, cntB = MINF_LL;
+    rep(i,N) {
+        sA.insert(a[i]);
+        cntA = max(cntA, numMp[a[i]]);
+        rA[i] = make_pair(sA.size() ,cntA);
+    }
+
+    rep(i,N) {
+        sB.insert(b[i]);
+        cntB = max(cntB, numMp[b[i]]);
+        rB[i] = make_pair(sB.size() ,cntB);
+    }
+
+
+    cin >> Q;
+
+    rep(i,Q) {
+        ll x, y;
+        cin >> x >> y;
+        x--; y--;
+        if( rA[x].first == rB[y].first && rA[x].second == rB[y].second ) cout << "Yes" << endl;
+        else  cout << "No" << endl;
+    }
 }
 
 int main()
