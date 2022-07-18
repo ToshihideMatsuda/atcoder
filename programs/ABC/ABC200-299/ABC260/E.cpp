001695c8665f1e113d2a8a889b9758af59d1bdee
mazdato <toshihide.matsuda@gmail.com>
2022-07-18 23:22:28 +0900

    ABC260 E

--- programs/ABC/ABC200-299/ABC260/E.cpp
+++ programs/ABC/ABC200-299/ABC260/E.cpp
@@ -22,9 +22,105 @@ typedef long long ll;
 
 
 void solve() {
+  ll N, M; cin >> N >> M;
+  ll A[N], B[N];
+  rep(i,N) {
+    cin >> A[i] >> B[i];
+    A[i] --; B[i] --;
+  } 
+  ll f[M+2];
+  rep(i,M+2)f[i] = 0;
+  ll l = 0, r = 0;
+  while(l < M) {
+    ll inf = INF_LL;
+    ll lb = r-1, ub = M+1;
+    while(ub - lb > 1) {
+      bool ok = true;
+      ll tmp = INF_LL;
+
+      ll mid = (lb + ub) / 2;
+      rep(i,N) {
+        bool Aok = l <= A[i] && A[i] <= mid;
+        bool Bok = l <= B[i] && B[i] <= mid;
+
+        if(Aok & Bok) tmp = min(tmp,max(A[i],B[i]));
+        else if(Aok) tmp = min(tmp, A[i]);
+        else if(Bok) tmp = min(tmp, B[i]);
+
+        if(!Aok && !Bok){ 
+          ok = false;
+          break;
+        }
+      }
+      if(ok) {
+        ub = mid;
+        inf = tmp;
+      } else {
+        lb = mid;
+      }
+    }
+    r = ub;
+    if(r >= M) break;
+    reps(i,l,inf+1) {
+      f[r - i + 1] ++;
+      f[M - i + 1] --;
+    }
+    l = inf+1;
+  }
+
+  rep(i,M) f[i+1] = f[i+1] + f[i]; 
+
+  reps(i,1,M+1) cout << f[i] << " ";
+  cout << endl;
+
 }
 
 
+void solve2() {
+  ll N, M; cin >> N >> M;
+  ll A[N], B[N];
+  rep(i,N) {
+    cin >> A[i] >> B[i];
+    A[i] --; B[i] --;
+  } 
+  ll f[M+2];
+  rep(i,M+2)f[i] = 0;
+  ll l = 0, r = 0;
+  while(l < M) {
+    ll inf = INF_LL;
+    while(r < M) {
+      bool ok = true;
+      inf = INF_LL;
+      rep(i,N) {
+        bool Aok = l <= A[i] && A[i] <= r;
+        bool Bok = l <= B[i] && B[i] <= r;
+ 
+        if(Aok & Bok) inf = min(inf,max(A[i],B[i]));
+        else if(Aok) inf = min(inf, A[i]);
+        else if(Bok) inf = min(inf, B[i]);
+ 
+        if(!Aok && !Bok){ 
+          ok = false;
+          break;
+        }
+      }
+      if(ok) break;
+      r++;
+    }
+    if(r >= M) break;
+    reps(i,l,inf+1) {
+      f[r - i + 1] ++;
+      f[M - i + 1] --;
+    }
+    l = inf+1;
+  }
+ 
+  rep(i,M) f[i+1] = f[i+1] + f[i]; 
+ 
+  reps(i,1,M+1) cout << f[i] << " ";
+  cout << endl;
+ 
+}
 int main()
 {
   solve();
