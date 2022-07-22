6728f748d6332734665ebce85e0240ca7a5c0cb7
mazdato <toshihide.matsuda@gmail.com>
2022-07-22 09:03:36 +0900

    ABC260 F

--- programs/ABC/ABC200-299/ABC260/F.cpp
+++ programs/ABC/ABC200-299/ABC260/F.cpp
@@ -22,6 +22,36 @@ typedef long long ll;
 
 
 void solve() {
+  ll S, T, M; cin >> S >> T >> M;
+  vector<ll> v(M), u(M);
+  vector<ll> L[S+5];
+
+  rep(i,M) cin >> u[i] >> v[i];
+  rep(i,M) L[u[i]].push_back(v[i] - S);
+
+  ll Mt[T+5][T+5];
+  rep(i,T+5) rep(j,T+5) Mt[i][j] = -1;
+
+  rep(i,S+1) {
+    if( L[i].size() < 2) continue;
+    for(ll x: L[i]) for(ll y : L[i])if(x < y) {
+      if(Mt[x][y] == -1) {
+        Mt[x][y] = i;
+      } else {
+        cout << x + S<< " ";
+        cout << Mt[x][y] << " ";
+        cout << y + S<< " ";
+        cout << i << endl;
+        return;
+      }
+    }
+  }
+
+  cout << -1 << endl;
+
+
+
+
 }
 
 
