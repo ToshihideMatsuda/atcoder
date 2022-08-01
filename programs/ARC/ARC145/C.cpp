a0f680bd8e56b11f52a626b792b79a3e129d79f2
mazdato <toshihide.matsuda@gmail.com>
2022-08-01 10:56:50 +0900

    ARC145 C & modCatalan

--- programs/ARC/ARC145/C.cpp
+++ programs/ARC/ARC145/C.cpp
@@ -1,6 +1,25 @@
 
+#include <bits/stdc++.h> 
+#include <unordered_map>
+using namespace std;
+
 typedef long long ll;
+
+#define rep(i,n)     for(ll i = 0; i < n; i++ ) 
+#define reps(i,m,n)  for(ll i = m; i < n; i++ ) 
+#define max(a,b)   (a>b?a:b)
+#define max3(a,b,c) max(a,max(b,c))
+#define min(a,b)   (a<b?a:b)
+#define min3(a,b,c) min(a,min(b,c))
+
+#define INF 1 << 30
+#define MINF -1*(1 << 30)
+#define INF_LL 1LL << 60
+#define MINF_LL - (1LL << 60)
 #define MOD 998244353
+
+#define MAX_N 2*100000+5
+
 
 ll modPow(ll x, ll a) {
   if (a == 1) return x;
@@ -33,3 +52,10 @@ ll modCatalan(ll n) {
   return (a * modInv(n+1))  % MOD;
 }
 
+
+int main()
+{
+    ll N; cin >> N;
+    cout << (modPerm(N,N) * (modCatalan(N) * modPow(2,N) % MOD ) % MOD) << endl;
+	return 0;
+}
