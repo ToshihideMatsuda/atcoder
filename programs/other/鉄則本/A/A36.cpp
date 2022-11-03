22c5bf1ad20314a7b411c00ec59697306987d0ef
mazdato <toshihide.matsuda@gmail.com>
2022-11-03 23:57:38 +0900

    A36 upsolve

---
+++ programs/other/鉄則本/A/A36.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h> 
+#include <unordered_map>
+using namespace std;
+
+typedef long long ll;
+
+#define rep(i,n)     for(ll i = 0; i < n; i++ ) 
+#define reps(i,m,n)  for(ll i = m; i < n; i++ ) 
+#define rev(i,n)     for(ll i = n; i > -1; i--) 
+#define revs(i,m,n)  for(ll i = m; i > n; i--) 
+#define MAX(a,b)   (a>b?a:b)
+#define MAX3(a,b,c) max(a,MAX(b,c))
+#define MIN(a,b)   (a<b?a:b)
+#define MIN3(a,b,c) min(a,MIN(b,c))
+
+#define INF (1 << 30)
+#define MINF -1*(1 << 30)
+#define INF_LL (1LL << 60)
+#define MINF_LL - (1LL << 60)
+#define MOD 998244353
+#define INV2 499122177 // inverse of 2 in MOD
+
+#define MAX_N 300000 + 5
+
+
+
+
+void solve() {
+  ll N, K; cin >> N >> K;
+  if(K < 2*(N-1)) cout << "No" <<endl;
+  else if(K % 2 == 1)  cout << "No" <<endl;
+  else {
+     cout << "Yes" <<endl;
+  }
+}
+
+
+int main()
+{
+  solve();
+	return 0;
+}
