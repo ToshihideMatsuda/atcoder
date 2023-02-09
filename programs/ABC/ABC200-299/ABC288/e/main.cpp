7d5bfc436fa090f4564fc90321fea1b0100d7a2d
mazdato <toshihide.matsuda@gmail.com>
2023-02-09 22:38:16 +0900

    ABC288 e upsolve

--- programs/ABC/ABC200-299/ABC288/e/main.cpp
+++ programs/ABC/ABC200-299/ABC288/e/main.cpp
@@ -23,18 +23,61 @@ typedef unsigned long long ull;
 #define MOD 998244353
 #define INV2 499122177 // inverse of 2 in MOD
 
-#define MAX_N (2*100000+5)
-vector<ll> G[MAX_N];
-bool ck[MAX_N]; void clear() { rep(i,MAX_N) ck[i] = false; }
-void readG(ll M) { rep(i,M) { ll a, b; cin >> a >> b; G[a].push_back(b); G[b].push_back(a);} }
+#define MAX_N 1000000+5
+ll cost[5005][5005];//
+ll dp[5005][5005];
+//dp[m][n]:m-index res n
+void solve(){
+	
+	ll N,M;cin>>N>>M;
+	vector<ll>A(N+1),C(N+1);
+	set<ll>X;
+	rep(i,N)cin>>A[i+1];
+	rep(i,N)cin>>C[i+1];
+	rep(i,M){
+		ll x;
+		cin>>x;
+		X.insert(x);
+	}
+	
+	reps(i,1,N+1){
+		cost[i][1]=C[i];
+		reps(j,2,N+1){
+			cost[i][j]= min(cost[i][j-1],C[i-(j-1)]);
+		}
+		//reps(j,1,N+1)cout<<i<<","<<j<<"="<<cost[i][j]<<endl;
+	}
+  
+  ll cnt = 0;
+	reps(i,1,N+1){
+    dp[i][cnt] = dp[i-1][cnt];
+		reps(j,cnt+1,i+1){
+			dp[i][j]=dp[i-1][j-1]+A[i]+cost[i][j];
+			if(X.count(i)){
+			}
+			else if(j <= i-1){
+			  dp[i][j]=min(dp[i][j],dp[i-1][j]);
+			}
+			
+			//cout<<"dp["<<i<<"]["<<j<<"]="<<dp[i][j]<<endl;
+		}
 
-void solve() {
-
+			if(X.count(i)){
+        cnt++;
+			}
+		
+	}
+	
+	ll ans =INF_LL;
+	reps(i,X.size(),N+1){
+		//cout<<dp[N][i]<<endl;
+		ans=min(ans,dp[N][i]);
+	}
+	cout<< ans;
 }
 
-
-int main()
-{
-  solve();
-	return 0;
+int main(){
+    solve();
+    return 0;
 }
+
