6eda32f3b79c1fa6a5e3b05ad1cf66f252f458bc
mazdato <toshihide.matsuda@gmail.com>
2023-05-20 20:55:35 +0900

    update

--- programs/ABC/ABC300-399/ABC301/b/main.cpp
+++ programs/ABC/ABC300-399/ABC301/b/main.cpp
@@ -1,12 +1,6 @@
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-#include <map>
-#include <set>
-#include <queue>
-#include <algorithm>
-#include <iomanip>
-#include <unordered_map>
+#include <bits/stdc++.h> 
+#include <atcoder/all>
+using namespace atcoder;
 using namespace std;
 
 typedef long long ll;
@@ -20,33 +14,30 @@ typedef unsigned long long ull;
 #define MAX3(a,b,c) MAX(a,MAX(b,c))
 #define MIN(a,b)   (a<b?a:b)
 #define MIN3(a,b,c) MIN(a,MIN(b,c))
-#define out(s) cout << s << endl;
-#define out2(s) cout << s;
+#define out0(s) cout << s;
+#define out(s) out1(s)
+#define out1(s) cout << s << endl;
+#define out2(s,t) cout << s << t << endl;
+#define out3(s,t,r) cout << s << t << r << endl;
+#define out4(s,t,r,u) cout << s << t << r << u << endl;
+#define out5(s,t,r,u,o) cout << s << t << r << u << o << endl;
+#define outd(s) cout << setprecision(15) << s << endl;
+#define pb(s) push_back(s)
 
 #define INF (1 << 30)
 #define MINF -1*(1 << 30)
 #define INF_LL (1LL << 60)
 #define MINF_LL - (1LL << 60)
 #define MOD 998244353
+#define INV2 499122177 // inverse of 2 in MOD
 
 #define MAX_N (2*100000+5)
+vector<ll> G[MAX_N];
+bool ck[MAX_N]; void clear() { rep(i,MAX_N) ck[i] = false; }
+void readG(ll M) { rep(i,M) { ll a, b; cin >> a >> b; G[a].push_back(b); G[b].push_back(a);} }
 
 
 int main()
 {
-	ll n;cin>>n;
-	vector<ll>A(1);cin>>A[0];
-	reps(i,1,n){
-		ll a;cin>> a;
-		ll d = (a> A.back())?1:-1;
-		ll a0 = A.back()+d;
-		while(a0!=a){
-			A.push_back(a0);
-			a0+=d;
-		}
-		A.push_back(a);
-	}
-	rep(i,A.size())cout<<A[i]<<" ";
-	
 	return 0;
 }
--- programs/ABC/ABC300-399/ABC301/c/main.cpp
+++ programs/ABC/ABC300-399/ABC301/c/main.cpp
@@ -1,12 +1,6 @@
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-#include <map>
-#include <set>
-#include <queue>
-#include <algorithm>
-#include <iomanip>
-#include <unordered_map>
+#include <bits/stdc++.h> 
+#include <atcoder/all>
+using namespace atcoder;
 using namespace std;
 
 typedef long long ll;
@@ -20,52 +14,30 @@ typedef unsigned long long ull;
 #define MAX3(a,b,c) MAX(a,MAX(b,c))
 #define MIN(a,b)   (a<b?a:b)
 #define MIN3(a,b,c) MIN(a,MIN(b,c))
-#define out(s) cout << s << endl;
-#define out2(s) cout << s;
+#define out0(s) cout << s;
+#define out(s) out1(s)
+#define out1(s) cout << s << endl;
+#define out2(s,t) cout << s << t << endl;
+#define out3(s,t,r) cout << s << t << r << endl;
+#define out4(s,t,r,u) cout << s << t << r << u << endl;
+#define out5(s,t,r,u,o) cout << s << t << r << u << o << endl;
+#define outd(s) cout << setprecision(15) << s << endl;
+#define pb(s) push_back(s)
 
 #define INF (1 << 30)
 #define MINF -1*(1 << 30)
 #define INF_LL (1LL << 60)
 #define MINF_LL - (1LL << 60)
 #define MOD 998244353
+#define INV2 499122177 // inverse of 2 in MOD
 
 #define MAX_N (2*100000+5)
-ll ch[2][26+1];
+vector<ll> G[MAX_N];
+bool ck[MAX_N]; void clear() { rep(i,MAX_N) ck[i] = false; }
+void readG(ll M) { rep(i,M) { ll a, b; cin >> a >> b; G[a].push_back(b); G[b].push_back(a);} }
 
 
 int main()
 {
-	string S[2];cin>> S[0]>>S[1];
-	
-	rep(s,2)rep(i,S[s].size())if(S[s][i]=='@')ch[s][26]++;else ch[s][S[s][i]-'a']++;
-	
-	int atcoder0=0;
-	int atcoder1=0;
-	
-	
-	rep(i,26){
-	if(
-	i+'a'=='a' ||
-	i+'a'=='t' ||
-	i+'a'=='c' ||
-	i+'a'=='o' ||
-	i+'a'=='d' ||
-	i+'a'=='e' ||
-	i+'a'=='r'){
-		if(ch[0][i]>ch[1][i])
-		    atcoder0+=abs(ch[0][i]-ch[1][i]);
-		else
-		    atcoder1+=abs(ch[0][i]-ch[1][i]);
-	}else{
-		if(ch[0][i]-ch[1][i]!=0){
-			cout<<"No";return 0;
-		}
-	}
-	}
-	
-	
-	if(atcoder0<=ch[1][26]&&
-	   atcoder1<=ch[0][26])cout<<"Yes";
-	else cout <<"No";
 	return 0;
 }
