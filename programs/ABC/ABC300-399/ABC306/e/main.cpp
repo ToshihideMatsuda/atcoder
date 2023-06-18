ab29f0250fb602ccb57c54dd25d3aca563faf9a1
mazdato <toshihide.matsuda@gmail.com>
2023-06-18 17:32:40 +0900

    ABC306 update

---
+++ programs/ABC/ABC300-399/ABC306/e/main.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h> 
+#include <atcoder/all>
+using namespace atcoder;
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define rep(i,n)     for(ll i = 0; i < n; i++ ) 
+#define reps(i,m,n)  for(ll i = m; i < n; i++ ) 
+#define rev(i,n)     for(ll i = n; i > -1; i--) 
+#define revs(i,m,n)  for(ll i = m; i > n; i--) 
+#define MAX(a,b)   (a>b?a:b)
+#define MAX3(a,b,c) MAX(a,MAX(b,c))
+#define MIN(a,b)   (a<b?a:b)
+#define MIN3(a,b,c) MIN(a,MIN(b,c))
+#define out0(s) cout << s;
+#define out(s) out1(s)
+#define out1(s) cout << s << endl;
+#define out2(s,t) cout << s << t << endl;
+#define out3(s,t,r) cout << s << t << r << endl;
+#define out4(s,t,r,u) cout << s << t << r << u << endl;
+#define out5(s,t,r,u,o) cout << s << t << r << u << o << endl;
+#define outd(s) cout << setprecision(15) << s << endl;
+#define pb(s) push_back(s)
+
+#define INF (1 << 30)
+#define MINF -1*(1 << 30)
+#define INF_LL (1LL << 60)
+#define MINF_LL - (1LL << 60)
+#define MOD 998244353
+#define INV2 499122177 // inverse of 2 in MOD
+
+#define MAX_N (2*100000+5)
+vector<ll> G[MAX_N];
+bool ck[MAX_N]; void clear() { rep(i,MAX_N) ck[i] = false; }
+void readG(ll M) { rep(i,M) { ll a, b; cin >> a >> b; G[a].push_back(b); G[b].push_back(a);} }
+
+
+int main()
+{
+	ll N, K, Q; cin >> N >> K >> Q;
+
+	vector<ll> Y(N+1,0);
+
+	multiset<ll> L; // in K
+	multiset<ll> R; // out K
+	rep(i, N) if(i < K) L.insert(0); else R.insert(0);
+
+
+	
+	ll sum = 0;
+	rep(_,Q) {
+		ll x, y;
+		cin>> x >> y;
+		ll bf = Y[x];
+		Y[x] = y;
+
+		//Kの境界値
+		ll a = *(L.begin());
+		if(bf < a) {
+			// bfはRに入っていた
+			R.erase(R.find(bf));
+		} else {
+			// bfはLに入っていた
+			L.erase(L.find(bf));
+			sum -= bf;
+		}
+
+		//Kの境界値
+		if(L.size() == 0) {
+			L.insert(y);
+			sum += y;
+		} else {
+			a = *(L.begin());
+			if(y < a) {
+				// yはRに入る
+				R.insert(y);
+			} else {
+				// bfはLに入っていた
+				L.insert(y);
+				sum += y;
+			}
+		}
+		
+		if(K < L.size()) {
+			ll a = *(L.begin());
+			sum -= a;
+			L.erase(L.begin());
+			R.insert(a);
+		} else if(L.size() < K ){
+			ll a = *(R.rbegin());
+			sum += a;
+			L.insert(a);
+			R.erase(R.find(a));
+		}
+
+		if(R.size() > 0 ) {
+			ll l = *(L.begin());
+			ll r = *(R.rbegin());
+			if(l < r) {
+			L.erase(L.begin());
+			R.erase(R.find(r));
+
+			L.insert(r);
+			R.insert(l);
+			sum += r-l;
+		}
+		}
+
+		out(sum)
+
+
+	}
+
+	return 0;
+}
