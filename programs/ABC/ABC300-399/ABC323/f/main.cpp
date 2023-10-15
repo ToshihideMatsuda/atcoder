d5ca5735a7103c6a90cf7518ab569a0acdbf4903
mazdato <toshihide.matsuda@gmail.com>
2023-10-15 23:33:10 +0900

    ABC323

---
+++ programs/ABC/ABC300-399/ABC323/f/main.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h> 
+#include <atcoder/all>
+
+using namespace atcoder;
+using namespace std;
+// 多倍長テンプレ（デバッグだとダメかも）
+/* ---------------------- ここから ---------------------- */
+#include <boost/multiprecision/cpp_dec_float.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+namespace mp = boost::multiprecision;
+// 任意長整数型
+using bll = mp::cpp_int;
+// 仮数部が10進数で1024桁の浮動小数点数型(TLEしたら小さくする)
+using real = mp::number<mp::cpp_dec_float<1024>>;
+/* ---------------------- ここまで ---------------------- */
+
+typedef long long ll;
+
+#define rep(i,n)     for(ll i = 0; i < n; i++ ) 
+#define reps(i,m,n)  for(ll i = m; i < n; i++ ) 
+#define rev(i,n)     for(ll i = n; i > -1; i--) 
+#define revs(i,m,n)  for(ll i = m; i > n; i--) 
+#define MAX(a,b)   (a>b?a:b)
+#define MAX3(a,b,c) MAX(a,MAX(b,c))
+#define MIN(a,b)   (a<b?a:b)
+#define MIN3(a,b,c) MIN(a,MIN(b,c))
+#define out0(s) cout << s;
+#define out(s)  cout << s << endl;
+#define outd(s) cout << setprecision(15) << s << endl;
+#define pb(s) push_back(s)
+/*
+	vector<ll> X = {4,7,3,1,7,9,4};
+	SORT(X, <)
+	OUT(X,",") //1,3,4,4,7,7,9,
+
+	SORT(X, >)
+	OUT(X," ") //9 7 7 4 4 3 1
+*/
+#define SORT(A,R) std::sort((A).begin(),(A).end(), [](auto const& x, auto const& y) { return x R y; });
+#define OUT(A,separate) for(auto a : A) { cout << a << separate; }
+
+
+#define INF (1 << 30)
+#define MINF -1*(1 << 30)
+#define INF_LL (1LL << 60)
+#define MINF_LL - (1LL << 60)
+#define MOD 998244353
+
+#define MAX_N (2*100000+5)
+vector<ll> G[MAX_N];
+bool ck[MAX_N]; void clear() { rep(i,MAX_N) ck[i] = false; }
+void readG(ll M) { rep(i,M) { ll a, b; cin >> a >> b; G[a].push_back(b); G[b].push_back(a);} }
+
+
+int main()
+{
+	ll xa,ya,xb,yb,xc,yc;
+	cin >> xa >> ya >> xb >> yb >> xc >> yc;
+
+	vector<vector<ll>> pos = { {-1,0}, {0,-1}, {1,0}, {0,1} };
+	queue<pair<ll,ll>> Q;
+	if(xa != xb && ya != yb) {
+		rep(i,4) {
+			Q.push({abs(xa - (xb + pos[i][0])) + abs(ya - (yb + pos[i][1])) , i });
+		}
+	} else if(xa == xb) {
+		Q.push({abs(xa - (xb + pos[0][0])) + abs(ya - (yb + pos[0][1])) , 0 });
+		Q.push({abs(xa - (xb + pos[2][0])) + abs(ya - (yb + pos[2][1])) , 2 });
+		if(ya < yb) {
+			Q.push({abs(xa - (xb + pos[1][0])) + abs(ya - (yb + pos[1][1])) , 1 });
+			Q.push({abs(xa - (xb + pos[3][0])) + abs(ya - (yb + pos[3][1])) + 2 , 3 });
+		} else {
+			Q.push({abs(xa - (xb + pos[1][0])) + abs(ya - (yb + pos[1][1])) + 2 , 1 });
+			Q.push({abs(xa - (xb + pos[3][0])) + abs(ya - (yb + pos[3][1])) , 3 });
+		}
+	} else {
+		Q.push({abs(xa - (xb + pos[1][0])) + abs(ya - (yb + pos[1][1])) , 1 });
+		Q.push({abs(xa - (xb + pos[3][0])) + abs(ya - (yb + pos[3][1])) , 3 });
+		if(xa < xb) {
+			Q.push({abs(xa - (xb + pos[0][0])) + abs(ya - (yb + pos[0][1])) , 0 });
+			Q.push({abs(xa - (xb + pos[2][0])) + abs(ya - (yb + pos[2][1])) + 2 , 2 });
+		} else {
+			Q.push({abs(xa - (xb + pos[0][0])) + abs(ya - (yb + pos[0][1])) + 2, 0 });
+			Q.push({abs(xa - (xb + pos[2][0])) + abs(ya - (yb + pos[2][1])), 2 });
+		}
+	}
+	ll ans = INF_LL;
+	while(Q.size()>0) {
+		auto [s, i] = Q.front(); Q.pop();
+		
+		ll dx = xc - xb, dy = yc - yb;
+		ll ddx = dx > 0 ? 1 : dx == 0 ? 0 : -1;
+		ll ddy = dy > 0 ? 1 : dy == 0 ? 0 : -1;
+		bool good = false;
+		ll addTwo = 0;
+		if((-1* pos[i][0] == ddx & ddx != 0) || (-1* pos[i][1] == ddy && ddy != 0)) { good = true; }
+		if(ddx != 0 && ddy != 0) { addTwo = 2; }
+		if(good) {
+			ll v = s + addTwo + abs(dx) + abs(dy);
+			ans = MIN(ans, v);
+		}
+	}
+	out(ans)
+
+
+	return 0;
+}
