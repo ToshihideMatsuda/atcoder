35e5c4bae783bdbf1dcb1af90491f825cd006209
mazdato <toshihide.matsuda@gmail.com>
2023-11-21 00:03:39 +0900

    update

---
+++ programs/ABC/ABC300-399/ABC328/e/main.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h> 
+#include <atcoder/all>
+
+using namespace atcoder;
+using namespace std;
+// 多倍長テンプレ（デバッグだとダメかも）
+/* ---------------------- ここから ---------------------- */
+#include <boost/multiprecision/cpp_dec_float.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+namespace mp = boost::multiprecision;
+// 任意長整数型
+using bll = mp::cpp_int;
+// 仮数部が10進数で1024桁の浮動小数点数型(TLEしたら小さくする)
+using real = mp::number<mp::cpp_dec_float<1024>>;
+/* ---------------------- ここまで ---------------------- */
+
+typedef long long ll;
+
+#define rep(i,n)     for(ll i = 0; i < n; i++ ) 
+#define reps(i,m,n)  for(ll i = m; i < n; i++ ) 
+#define rev(i,n)     for(ll i = n; i > -1; i--) 
+#define revs(i,m,n)  for(ll i = m; i > n; i--) 
+#define MAX(a,b)   (a>b?a:b)
+#define MAX3(a,b,c) MAX(a,MAX(b,c))
+#define MIN(a,b)   (a<b?a:b)
+#define MIN3(a,b,c) MIN(a,MIN(b,c))
+#define out0(s) cout << s;
+#define out(s)  cout << s << endl;
+#define outd(s) cout << setprecision(15) << s << endl;
+#define pb(s) push_back(s)
+/*
+	vector<ll> X = {4,7,3,1,7,9,4};
+	SORT(X, <)
+	OUT(X,",") //1,3,4,4,7,7,9,
+
+	SORT(X, >)
+	OUT(X," ") //9 7 7 4 4 3 1
+*/
+#define SORT(A,R) std::sort((A).begin(),(A).end(), [](auto const& x, auto const& y) { return x R y; });
+#define OUT(A,separate) for(auto a : A) { cout << a << separate; }
+
+
+#define INF (1 << 30)
+#define MINF -1*(1 << 30)
+#define INF_LL (1LL << 60)
+#define MINF_LL - (1LL << 60)
+#define MOD 998244353
+
+#define MAX_N (2*100000+5)
+vector<ll> G[MAX_N];
+bool ck[MAX_N]; void clear() { rep(i,MAX_N) ck[i] = false; }
+void readG(ll M) { rep(i,M) { ll a, b; cin >> a >> b; G[a].push_back(b); G[b].push_back(a);} }
+
+
+int main()
+{
+	ll N, M; cin >> N >> M;
+	string S, T; cin >> S >> T;
+
+
+	queue<ll> Q;
+	set<ll> s;
+	rep(i,N-M+1) {
+		bool ok = true;
+		rep(j,M) if(S[i+j]  != T[j]) ok = false;
+		if(ok) {
+			s.insert(i);
+			Q.push(i);
+		}
+	}
+
+	
+	while(Q.size() > 0) {
+		auto q = Q.front(); Q.pop();
+		rep(i,M) {
+			S[i+q] = '#';
+		}
+		reps(k, MAX(0,q-M), MIN(N-M+1, q+M)) {
+			bool ok = true;
+			rep(j,M) if(S[k+j]  == T[j] || S[k+j] ==  '#') continue; else ok = false;
+			if(ok && s.count(k) == false) {
+				s.insert(k);
+				Q.push(k);
+			}
+		}
+	}
+
+	bool ok = true;
+	rep(i,N) if(S[i] != '#') ok = false;
+	out( (ok? "Yes" : "No" ))
+
+	return 0;
+}
