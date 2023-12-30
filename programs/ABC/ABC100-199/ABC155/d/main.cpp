38d7316e99da9db374bef34da4b4dd056da6a050
mazdato <toshihide.matsuda@gmail.com>
2023-12-30 14:42:44 +0900

    ABC155

---
+++ programs/ABC/ABC100-199/ABC155/d/main.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h> 
+#include <atcoder/all>
+
+using namespace atcoder;
+using namespace std;
+// 多倍長テンプレ（デバッグだとダメかも）
+/* ---------------------- ここから ---------------------- */
+#include <boost/multiprecision/cpp_dec_float.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+namespace mp = boost::multiprecision;
+// 任意長整数型
+using bll = mp::cpp_int;
+// 仮数部が10進数で1024桁の浮動小数点数型(TLEしたら小さくする)
+using real = mp::number<mp::cpp_dec_float<1024>>;
+/* ---------------------- ここまで ---------------------- */
+
+typedef long long ll;
+
+#define rep(i,n)     for(ll i = 0; i < n; i++ ) 
+#define reps(i,m,n)  for(ll i = m; i < n; i++ ) 
+#define rev(i,n)     for(ll i = n; i > -1; i--) 
+#define revs(i,m,n)  for(ll i = m; i > n; i--) 
+#define MAX(a,b)   (a>b?a:b)
+#define MAX3(a,b,c) MAX(a,MAX(b,c))
+#define MIN(a,b)   (a<b?a:b)
+#define MIN3(a,b,c) MIN(a,MIN(b,c))
+#define out0(s) cout << s;
+#define out(s)  cout << s << endl;
+#define outd(s) cout << setprecision(15) << s << endl;
+#define pb(s) push_back(s)
+/*
+	vector<ll> X = {4,7,3,1,7,9,4};
+	SORT(X, <)
+	OUT(X,",") //1,3,4,4,7,7,9,
+
+	SORT(X, >)
+	OUT(X," ") //9 7 7 4 4 3 1
+*/
+#define SORT(A,R) std::sort((A).begin(),(A).end(), [](auto const& x, auto const& y) { return x R y; });
+#define OUT(A,separate) for(auto a : A) { cout << a << separate; }
+
+
+#define INF (1 << 30)
+#define MINF -1*(1 << 30)
+#define INF_LL (1LL << 60)
+#define MINF_LL - (1LL << 60)
+#define MOD 998244353
+
+#define MAX_N (2*100000+5)
+vector<ll> G[MAX_N];
+bool ck[MAX_N]; void clear() { rep(i,MAX_N) ck[i] = false; }
+void readG(ll M) { rep(i,M) { ll a, b; cin >> a >> b; G[a].push_back(b); G[b].push_back(a);} }
+
+
+int main()
+{
+	ll n,K;cin >> n >> K;
+	vector<ll> a(n+2);
+
+	reps(i,1,1+n) cin >> a[i];
+	a[0] = MINF_LL; a[n+1] = INF_LL;
+	sort(a.begin(),a.end());
+
+	ll lb = MINF_LL, ub = INF_LL;
+
+	while(ub - lb > 1) {
+		ll k = (lb+ub)/2;
+
+		ll cnt = 0;
+
+		reps(i,1,1+n) {
+			if(a[i] == 0) {
+				if(k <= 0) cnt += n-1;
+			} else if(a[i] < 0) {
+				ll lb0 = n, ub0 = 0;
+				while(ub0-lb0 > 1) {
+					ll mb = (lb0+ub0)/2;
+
+					if(a[mb]*a[i] <= k) {
+						lb0 = mb;
+					} else {
+						ub0 = mb;
+					}
+				}
+
+				if(i <= lb0) lb0--;
+				cnt += lb0;
+
+			} else { //0 < a[i]
+				ll lb0 = 0, ub0 = n;
+				while(ub0-lb0 > 1) {
+					ll mb = (lb0+ub0)/2;
+
+					if(a[mb]*a[i] <= k) {
+						lb0 = mb;
+					} else {
+						ub0 = mb;
+					}
+				}
+
+				if(i <= lb0) lb0--;
+				cnt += lb0;
+
+			}
+		}
+
+		if(cnt <= K) ub = cnt;
+		else lb = cnt;
+
+	}
+
+	out(ub);
+
+
+	return 0;
+}
