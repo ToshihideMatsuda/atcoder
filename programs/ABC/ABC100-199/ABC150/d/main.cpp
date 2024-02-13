0022f422ad7162b2260542cd760820cdf8cd1485
mazdato <toshihide.matsuda@gmail.com>
2024-02-13 23:29:45 +0900

    update ABC150

---
+++ programs/ABC/ABC100-199/ABC150/d/main.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h> 
+#include <atcoder/all>
+
+using namespace atcoder;
+using namespace std;
+// 多倍長テンプレ（デバッグだとダメかも）
+/* ---------------------- ここから ---------------------- */
+#include <boost/multiprecision/cpp_dec_float.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+namespace mp = boost::multiprecision;
+// 任意長整数型
+using bll = mp::cpp_int;
+// 仮数部が10進数で1024桁の浮動小数点数型(TLEしたら小さくする)
+using real = mp::number<mp::cpp_dec_float<1024>>;
+/* ---------------------- ここまで ---------------------- */
+
+typedef long long ll;
+
+#define rep(i,n)     for(ll i = 0; i < n; i++ ) 
+#define reps(i,m,n)  for(ll i = m; i < n; i++ ) 
+#define rev(i,n)     for(ll i = n; i > -1; i--) 
+#define revs(i,m,n)  for(ll i = m; i > n; i--) 
+#define MAX(a,b)   (a>b?a:b)
+#define MAX3(a,b,c) MAX(a,MAX(b,c))
+#define MIN(a,b)   (a<b?a:b)
+#define MIN3(a,b,c) MIN(a,MIN(b,c))
+#define out0(s) cout << s;
+#define out(s)  cout << s << endl;
+#define outd(s) cout << setprecision(15) << s << endl;
+#define pb(s) push_back(s)
+/*
+	vector<ll> X = {4,7,3,1,7,9,4};
+	SORT(X, <)
+	OUT(X,",") //1,3,4,4,7,7,9,
+
+	SORT(X, >)
+	OUT(X," ") //9 7 7 4 4 3 1
+*/
+#define SORT(A,R) std::sort((A).begin(),(A).end(), [](auto const& x, auto const& y) { return x R y; });
+#define OUT(A,separate) for(auto a : A) { cout << a << separate; }
+
+
+#define INF (1 << 30)
+#define MINF -1*(1 << 30)
+#define INF_LL (1LL << 60)
+#define MINF_LL - (1LL << 60)
+#define MOD 998244353
+
+#define MAX_N (2*100000+5)
+vector<ll> G[MAX_N];
+bool ck[MAX_N]; void clear() { rep(i,MAX_N) ck[i] = false; }
+void readG(ll M) { rep(i,M) { ll a, b; cin >> a >> b; G[a].push_back(b); G[b].push_back(a);} }
+
+
+inline long long mod(long long a, long long m) {
+    return (a % m + m) % m;
+}
+
+long long extGcd(long long a, long long b, long long &p, long long &q) {  
+    if (b == 0) { p = 1; q = 0; return a; }  
+    long long d = extGcd(b, a%b, q, p);  
+    q -= a/b * p;  
+    return d;  
+}
+
+// 中国剰余定理
+// リターン値を (r, m) とすると解は x ≡ r (mod. m)
+// 解なしの場合は (0, -1) をリターン
+pair<long long, long long> ChineseRem(const vector<long long> &b, const vector<long long> &m) {
+  long long r = 0, M = 1;
+  for (int i = 0; i < (int)b.size(); ++i) {
+    long long p, q;
+    long long d = extGcd(M, m[i], p, q); // p is inv of M/d (mod. m[i]/d)
+    if ((b[i] - r) % d != 0) return make_pair(0, -1);
+    long long tmp = (b[i] - r) / d * p % (m[i]/d);
+    r += M * tmp;
+    M *= m[i]/d;
+  }
+  return make_pair(mod(r, M), M);
+}
+
+int main()
+{
+	ll N, M; cin >> N >> M;
+	vector<ll> a(N); rep(i,N) cin >> a[i];
+	vector<ll> a_2(N); rep(i,N) a_2[i] = a[i]/2;
+
+	auto [r,m] = ChineseRem(a_2,a);
+
+	ll ans = 0;
+	if(r == 0 && m == -1) {
+		ans = 0;
+	} else {
+		ans = M/m;
+		if(1  <= r && r <= M%m) ans += 1;
+	}
+	out(ans)
+
+
+	return 0;
+}
