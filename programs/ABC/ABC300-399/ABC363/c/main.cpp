73fdd0419e85b7a7b09e2b37d1fde63350bb07a2
mazdato <toshihide.matsuda@gmail.com>
2024-07-20 23:15:45 +0900

    update ABC363

---
+++ programs/ABC/ABC300-399/ABC363/c/main.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h> 
+#include <atcoder/all>
+
+using namespace atcoder;
+using namespace std;
+// 多倍長テンプレ（デバッグだとダメかも）
+/* ---------------------- ここから ---------------------- */
+#include <boost/multiprecision/cpp_dec_float.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+namespace mp = boost::multiprecision;
+// 任意長整数型
+using bll = mp::cpp_int;
+// 仮数部が10進数で1024桁の浮動小数点数型(TLEしたら小さくする)
+using real = mp::number<mp::cpp_dec_float<1024>>;
+/* ---------------------- ここまで ---------------------- */
+
+typedef long long ll;
+
+#define rep(i,n)     for(ll i = 0; i < n; i++ ) 
+#define reps(i,m,n)  for(ll i = m; i < n; i++ ) 
+#define rev(i,n)     for(ll i = n; i > -1; i--) 
+#define revs(i,m,n)  for(ll i = m; i > n; i--) 
+#define MAX(a,b)   (a>b?a:b)
+#define MAX3(a,b,c) MAX(a,MAX(b,c))
+#define MIN(a,b)   (a<b?a:b)
+#define MIN3(a,b,c) MIN(a,MIN(b,c))
+#define out0(s) cout << s;
+#define out(s)  cout << s << endl;
+#define outd(s) cout << setprecision(15) << s << endl;
+#define pb(s) push_back(s)
+/*
+	vector<ll> X = {4,7,3,1,7,9,4};
+	SORT(X, <)
+	OUT(X,",") //1,3,4,4,7,7,9,
+
+	SORT(X, >)
+	OUT(X," ") //9 7 7 4 4 3 1
+*/
+#define SORT(A,R) std::sort((A).begin(),(A).end(), [](auto const& x, auto const& y) { return x R y; });
+#define OUT(A,separate) for(auto a : A) { cout << a << separate; }
+
+
+#define INF (2147483647)
+#define MINF (-2147483648)
+#define INF_LL  (9223372036854775807LL)
+#define MINF_LL (-9223372036854775807LL)
+#define MOD 998244353
+
+#define MAX_N (2*100000+5)
+vector<ll> G[MAX_N];
+bool ck[MAX_N]; void clear() { rep(i,MAX_N) ck[i] = false; }
+void readG(ll M) { rep(i,M) { ll a, b; cin >> a >> b; G[a].push_back(b); G[b].push_back(a);} }
+
+
+int main()
+{
+	ll N, K; cin >> N >> K;
+	string S; cin >> S;
+	vector<ll> s(N);
+	rep(i,N) s[i] = i;
+	ll cnt = 0;
+	set<string> ss; 
+	set<string> ck;
+	do {
+
+		bool noadd = false;
+
+		rep(i, N-K+1){
+			bool kaibun = true;
+			for(int j = 0; j < K/2+1; j ++) {
+				if(S[s[i+j]] != S[s[i+K-1-j]]){
+					kaibun = false;
+					break;
+				}
+			}
+			if(kaibun) {
+				noadd = true;
+				break;
+			}
+		} 
+
+		if(noadd == false) {
+			string T;
+			rep(i,N) T += S[s[i]];
+			ss.insert(T);
+		}    
+	} while(next_permutation(s.begin(),s.end()));
+
+	out(ss.size());
+	return 0;
+}
