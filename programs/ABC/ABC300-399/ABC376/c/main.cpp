c4f1612c02419d7e6a1c55a09885f8d24e9da5cf
mazdato <toshihide.matsuda@gmail.com>
2024-11-01 10:03:24 +0900

    update

---
+++ programs/ABC/ABC300-399/ABC376/c/main.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h> 
+#include <atcoder/all>
+
+using namespace atcoder;
+using namespace std;
+// 多倍長テンプレ（デバッグだとダメかも）
+/* ---------------------- ここから ---------------------- */
+#include <boost/multiprecision/cpp_dec_float.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+namespace mp = boost::multiprecision;
+// 任意長整数型
+using bll = mp::cpp_int;
+// 仮数部が10進数で1024桁の浮動小数点数型(TLEしたら小さくする)
+using real = mp::number<mp::cpp_dec_float<1024>>;
+/* ---------------------- ここまで ---------------------- */
+
+typedef long long ll;
+
+#define rep(i,n)     for(ll i = 0; i < n; i++ ) 
+#define reps(i,m,n)  for(ll i = m; i < n; i++ ) 
+#define rev(i,n)     for(ll i = n; i > -1; i--) 
+#define revs(i,m,n)  for(ll i = m; i > n; i--) 
+#define MAX(a,b)   (a>b?a:b)
+#define MAX3(a,b,c) MAX(a,MAX(b,c))
+#define MIN(a,b)   (a<b?a:b)
+#define MIN3(a,b,c) MIN(a,MIN(b,c))
+#define out0(s) cout << s;
+#define out(s)  cout << s << endl;
+#define outd(s) cout << setprecision(15) << s << endl;
+#define pb(s) push_back(s)
+/*
+	vector<ll> X = {4,7,3,1,7,9,4};
+	SORT(X, <)
+	OUT(X,",") //1,3,4,4,7,7,9,
+
+	SORT(X, >)
+	OUT(X," ") //9 7 7 4 4 3 1
+*/
+#define SORT(A,R) std::sort((A).begin(),(A).end(), [](auto const& x, auto const& y) { return x R y; });
+#define OUT(A,separate) for(auto a : A) { cout << a << separate; }
+
+
+#define INF  (2000000000000000001LL)
+#define MINF (-2000000000000000001LL)
+#define MOD 998244353
+
+#define MAX_N (2*100000+5)
+vector<ll> G[MAX_N];
+bool ck[MAX_N]; void clear() { rep(i,MAX_N) ck[i] = false; }
+void readG(ll M) { rep(i,M) { ll a, b; cin >> a >> b; G[a].push_back(b); G[b].push_back(a);} }
+
+
+int main()
+{
+	ll N; cin >> N;
+	vector<ll> A(N), B(N-1);
+	rep(i,N) cin >> A[i];
+	rep(i,N-1) cin >> B[i];
+	sort(A.begin(),A.end());
+	sort(B.begin(),B.end());
+	rep(i,N-1) if(B[i] < A[i]) {
+		out(-1)
+		return 0;
+	}
+
+	ll x = A[N-1];
+
+	for(int i = N-1;i>0;i--) {
+		if(A[i] <= B[i-1]){
+			x = A[i-1];
+		} else {
+			break;
+		}
+	}
+	out(x);
+	return 0;
+}
